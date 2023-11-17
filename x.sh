106970549fc3d64949170c3f5ca7fe30d09fe873
odedev <odedev@outlook.com>
2023-11-17 10:02:10 +0800

    init

---
+++ x.sh
@@ -0,0 +1,19 @@
+
+
+# rush init
+
+mkdir apps
+mkdir packages
+
+mkdir packages/common-core
+mkdir packages/common-node
+mkdir packages/common-web
+
+mkdir packages/component-web
+mkdir packages/component-vue
+mkdir packages/component-react
+mkdir packages/component-angular
+
+mkdir doc
+
+npx create-docusaurus@latest document classic
