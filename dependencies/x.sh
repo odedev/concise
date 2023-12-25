f87451fd41a55fd4b44eae43c6280ca83d32b80a
odedev <odedev@outlook.com>
2023-12-25 18:16:51 +0800

    update

---
+++ dependencies/x.sh
@@ -0,0 +1,40 @@
+
+
+
+
+npm install --save uuid
+npm install --save nanoid
+
+
+
+npm install --save file-type
+
+npm install --save bwip-js
+npm install --save qrcode
+npm install --save qrcode-svg
+npm install --save zxing-wasm
+
+npm install --save exceljs
+
+npm install --save uri-js
+
+
+
+
+npm install --save @popperjs/core
+
+
+
+npm install --save ws
+
+npm install --save handlebars
+
+npm install --save bson
+npm install --save mongodb
+npm install --save mongodb
+
+npm install --save commander
+
+
+npm install --save-dev @types/uuid
+npm install --save-dev @types/qrcode
